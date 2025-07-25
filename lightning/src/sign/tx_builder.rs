0b8fbaddb31ba566fae7c2abb815903d8d932ee0
Leo Nash <hello@leonash.net>
2025-07-25 10:28:52 -0700

    Remove TxBuilder::commit_tx_fee_sat

--- lightning/src/sign/tx_builder.rs
+++ lightning/src/sign/tx_builder.rs
@@ -81,7 +81,6 @@ fn on_holder_tx_dust_exposure_msat(
 
 pub(crate) trait TxBuilder {
 	fn get_builder_stats(&self, htlcs: &[HTLCAmountHeading], nondust_htlcs: usize, feerate_per_kw: u32, dust_buffer_feerate: u32, excess_feerate: Option<u32>, max_dust_exposure_msat: u64, channel_type: &ChannelTypeFeatures, holder_dust_limit_satoshis: u64, counterparty_dust_limit_satoshis: u64) -> BuilderStats;
-	fn commit_tx_fee_sat(&self, feerate_per_kw: u32, nondust_htlc_count: usize, channel_type: &ChannelTypeFeatures) -> u64;
 	fn subtract_non_htlc_outputs(
 		&self, is_outbound_from_holder: bool, value_to_self_after_htlcs: u64,
 		value_to_remote_after_htlcs: u64, channel_type: &ChannelTypeFeatures,
@@ -223,11 +222,6 @@ impl TxBuilder for SpecTxBuilder {
 			on_holder_tx_dust_exposure_msat,
 			max_dust_exposure_msat,
 		}
-	}
-	fn commit_tx_fee_sat(
-		&self, feerate_per_kw: u32, nondust_htlc_count: usize, channel_type: &ChannelTypeFeatures,
-	) -> u64 {
-		commit_tx_fee_sat(feerate_per_kw, nondust_htlc_count, channel_type)
 	}
 	fn subtract_non_htlc_outputs(
 		&self, is_outbound_from_holder: bool, value_to_self_after_htlcs: u64,
