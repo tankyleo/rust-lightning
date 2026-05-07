9e12dd8cabd02e17be1a1e0bd246089d5660a2f6
Leo Nash <hello@leonash.net>
2026-05-07 05:26:59 +0000

    Use a real public nonce when sending a bogus reestablish

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -13319,6 +13319,15 @@ This indicates a bug inside LDK. Please report this error at https://github.com/
 				hash_map::Entry::Vacant(_) => {
 					log_debug!(logger, "Sending bogus ChannelReestablish for unknown channel to force channel closure",
 						);
+
+					const PUBLIC_NONCE: [u8; 66] = [
+						0x03, 0xf4, 0xa3, 0x61, 0xab, 0xd3, 0xd5, 0x05, 0x35, 0xbe, 0x08,
+						0x42, 0x1d, 0xbc, 0x73, 0xb0, 0xa8, 0xf5, 0x95, 0x65, 0x4a, 0xe3,
+						0x23, 0x8a, 0xfc, 0xaf, 0x25, 0x99, 0xf9, 0x4e, 0x25, 0x20, 0x4c,
+						0x03, 0x6b, 0xa1, 0x74, 0x21, 0x44, 0x33, 0xe2, 0x1f, 0x5c, 0xd0,
+						0xfc, 0xb1, 0x4b, 0x03, 0x8e, 0xb4, 0x0b, 0x05, 0xb7, 0xe7, 0xc8,
+						0x20, 0xdd, 0x21, 0xaa, 0x56, 0x8f, 0xdb, 0x0a, 0x9d, 0xe4, 0xd7,
+					];
 					// Unfortunately, lnd doesn't force close on errors
 					// (https://github.com/lightningnetwork/lnd/blob/abb1e3463f3a83bbb843d5c399869dbe930ad94f/htlcswitch/link.go#L2119).
 					// One of the few ways to get an lnd counterparty to force close is by
@@ -13343,7 +13352,7 @@ This indicates a bug inside LDK. Please report this error at https://github.com/
 							my_current_per_commitment_point: PublicKey::from_slice(&[2u8; 33]).unwrap(),
 							next_funding: None,
 							my_current_funding_locked: None,
-							next_local_nonces: vec![PublicNonce::from_byte_array(&[0u8; 66]).unwrap()],
+							next_local_nonces: vec![PublicNonce::from_byte_array(&PUBLIC_NONCE).unwrap()],
 						},
 					});
 					return Err(MsgHandleErrInternal::no_such_channel_for_peer(counterparty_node_id, msg.channel_id)
